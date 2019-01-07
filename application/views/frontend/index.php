1fc6df45d921aaa171281c99b73f50286f483b74
NaufalRivaldi <naufal.rivaldi33@gmail.com>
2019-01-07 20:48:15 +0800

    insert animate in article

--- application/views/frontend/index.php
+++ application/views/frontend/index.php
@@ -30,8 +30,7 @@
 	  <div class="center">
 	  	<h1 class="display-4">Selamat Datang di Konsultasi Warna Online</h1>
 	  	<h1 class="display-4"><?= APP_NAME ?> <img src="<?= base_url('assets/img/logo/logo.png') ?>" width="60px"></h1>
-	  	<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	  tempor incididunt ut labore et dolore magna aliqua.</p>
+	  	<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 	  </div>
 	</div>
 	<!-- jumbotron -->
@@ -39,37 +38,39 @@
 	<!-- artikel -->
 	<section id="artikel">
 		<div class="container">
-			<div class="row">
-				<h2>ARTIKEL KEMUDAHAN WARNA RUMAH</h2>
-			</div>
-			<div class="row">
-				<div class="col-sm">
-					<p class="text1">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					</p>
+			<div class="animate hidden">
+				<div class="row">
+					<h2>ARTIKEL KEMUDAHAN WARNA RUMAH</h2>
 				</div>
-				<div class="col-sm">
-					<img src="<?= base_url('assets/img/no-image.jpg') ?>" width="100%">
+				<div class="row">
+					<div class="col-sm">
+						<p class="text1">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+					</div>
+					<div class="col-sm">
+						<img src="<?= base_url('assets/img/no-image.jpg') ?>" width="100%">
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-sm">
-					<img src="<?= base_url('assets/img/no-image.jpg') ?>" width="100%">
-				</div>
-				<div class="col-sm">
-					<p class="text2">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					</p>
+				<div class="row">
+					<div class="col-sm">
+						<img src="<?= base_url('assets/img/no-image.jpg') ?>" width="100%">
+					</div>
+					<div class="col-sm">
+						<p class="text2">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -77,22 +78,22 @@
 	<!-- artikel -->
 
 	<!-- konsultasi -->
-	<center>
-		<h2>APA YANG ANDA BUTUHKAN?</h2>
-	</center>
 	<section id="konsultasi">
 		<div class="container">
 			<div class="row">
+				<h2>APA YANG ANDA BUTUHKAN?</h2>
+			</div>
+			<div class="row">
 				<div class="col-md-6 d-flex justify-content-center">
 					<a href="#" class="center">
-						<h3>Konsultasi Pemilihan Warna</h3>
 						<img src="<?= base_url('assets/img/konsultasi.png') ?>" width="60%">
+						<h3>Konsultasi Pemilihan Warna</h3>
 					</a>
 				</div>
 				<div class="col-md-6 d-flex justify-content-center">
 					<a href="https://www.cwabali.com/product/kategori" class="center">
-						<h3>Produk Kami</h3>
 						<img src="<?= base_url('assets/img/web-cwa.png') ?>" width="60%">
+						<h3>Produk Kami</h3>
 					</a>
 				</div>
 			</div>
@@ -102,22 +103,11 @@
 
 	<!-- footer -->
 	<section id="footer">
-		<div class="row">
-			<div class="col-md">
-				<p class="text-center">
-					&copy; Naufal Rivaldi - 2018
-				</p>
-			</div>
-			<div class="col-md">
-				<a href="#" class="big-icon">
-					<i class="fa fa-money-check"></i>
-				</a>
-				<a href="#" class="big-icon">
-					<i class="fab fa-facebook"></i>
-				</a>
-				<a href="#" class="big-icon">
-					<i class="fab fa-instagram"></i>
-				</a>
+		<div class="container">
+			<div class="row">
+				<div class="col-md">
+					<p class="text-footer">&copy; Naufal Rivaldi - 2018</p>	
+				</div>
 			</div>
 		</div>
 	</section>
